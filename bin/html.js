d5bd71e64745b8dcb6f86f66848da8b4f7d5532f
vivaxy <xyxuye2007@126.com>
2015-02-08 13:59:41 +0800

    change style label position to load style before content

--- bin/html.js
+++ bin/html.js
@@ -114,10 +114,10 @@ var template = function (files, hostname, port, pathname) {
         '<html>' +
         '<head>' +
         '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>' +
+        '<style>' + listStyle + '</style>' +
         '</head>' +
         '<body>' +
         list.join('') + '' +
-        '<style>' + listStyle + '</style>' +
         '</body>' +
         '</html>';
 };
