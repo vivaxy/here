c6409d1c2dfd79e22533ca1de73155e9709b677d
vivaxy <xyxuye2007@126.com>
2015-05-25 22:05:44 +0800

    watcher

--- test/index.js
+++ test/index.js
@@ -3,3 +3,19 @@
  * @author vivaxy
  */
 document.body.innerHTML += '<p>js ok</p>';
+
+var socket = new WebSocket('ws://127.0.0.1:13000');
+socket.onopen = function () {
+    console.log(arguments);
+};
+socket.onclose = function () {
+    console.log(arguments);
+};
+socket.onmessage = function (data) {
+    if (data.data === 'reload'){
+        location.reload();
+    }
+};
+socket.onerror = function () {
+    console.log(arguments);
+};
