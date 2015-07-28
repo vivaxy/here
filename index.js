6c7365e26fdd6c3503fb9454e44dde1b915c0547
vivaxy <xyxuye2007@126.com>
2015-07-28 20:27:20 +0800

    optimize log\nfix server port addition\nfix server parameter

--- index.js
+++ index.js
@@ -3,22 +3,19 @@
  * @since 150130 17:29
  * @author vivaxy
  */
-var util = require('util'),
-
-    chalk = require('chalk'),
+var chalk = require('chalk'),
     
+    log = require('./lib/log'),
     Server = require('./lib/server'),
     Watcher = require('./lib/watcher'),
     argument = require('./lib/argument'),
-
-    log = util.log,
 
     /**
      * main method
      */
     main = function () {
         if (argument.help) {
-            log(chalk.cyan('USAGE') +
+            log('serve-here: ' + chalk.cyan('USAGE') +
                 ' ' + 'here' + ' ' +
                 '[-p PORT]' + ' ' +
                 '[-d DIRECTORY]' + ' ' +
@@ -26,13 +23,12 @@ var util = require('util'),
                 '[-w INTERVAL(seconds)]' + ' ' +
                 '[-l]' + ' ' +
                 '[-v]' + '\n' +
-
-                chalk.cyan('-p, --port      ') + 'specify port; default 3000' + '\n' +
-                chalk.cyan('-d, --directory ') + 'specify root directory; default .' + '\n' +
-                chalk.cyan('-s, --silent    ') + 'will not open browser' + '\n' +
-                chalk.cyan('-w, --watch     ') + 'will watch html,js,css files; once changed, reload pages; default interval 0' + '\n' +
-                chalk.cyan('-l, --log       ') + 'output log' + '\n' +
-                chalk.cyan('-v, --version   ') + 'output version'
+                chalk.cyan('             -p, --port      ') + 'specify port; default 3000' + '\n' +
+                chalk.cyan('             -d, --directory ') + 'specify root directory; default .' + '\n' +
+                chalk.cyan('             -s, --silent    ') + 'will not open browser' + '\n' +
+                chalk.cyan('             -w, --watch     ') + 'will watch html,css,js files; once changed, reload pages; default interval 0' + '\n' +
+                chalk.cyan('             -l, --log       ') + 'output log' + '\n' +
+                chalk.cyan('             -v, --version   ') + 'output version'
             );
         } else if (argument.version) {
             log('serve-here: ' + require('./package.json').version);
@@ -47,10 +43,10 @@ var util = require('util'),
                     callback: function () {
                         watcherPort = watcher.getPort();
                         new Server({
-                            port: argument.port,
+                            log: argument.log,
+                            port: parseInt(argument.port),
                             watch: argument.watch,
                             silent: argument.silent,
-                            verbose: argument.verbose,
                             directory: argument.directory,
                             watcherPort: watcherPort
                         });
