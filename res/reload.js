c33b4ee50b533b684d6226b7a1746501d67dfd46
vivaxy <xyxuye2007@126.com>
2015-09-05 11:09:45 +0800

    reload.js

--- res/reload.js
+++ res/reload.js
@@ -2,8 +2,8 @@
  * @since 150524 20:36
  * @author vivaxy
  */
-new WebSocket('ws://127.0.0.1:13000').onmessage = function (data) {
-    if (data.data === 'reload') {
+new WebSocket('ws://127.0.0.1:13000').addEventListener('message', function (message) {
+    if (message.data === 'reload') {
         location.reload();
     }
-};
+});
