8c7f806a3517828c3f57aba4add28a6aebf73b34
vivaxy <xyxuye2007@126.com>
2015-11-06 14:34:50 +0800

    update test case

--- test/mocha.js
+++ test/mocha.js
@@ -8,39 +8,39 @@ var spawn = require('child_process').spawn;
 var packageJson = require('../package.json');
 
 describe('test terminal command `here`', function () {
-    var index;
+    var here;
     afterEach(function () {
-        index.kill();
+        here.kill();
     });
     it('`here` should output `[??:??:??.???] server : listen http://*.*.*.*:*/`', function (done) {
-        index = spawn('./index.js');
-        index.stdout.on('data', function (data) {
+        here = spawn('node', ['./index.js']);
+        here.stdout.on('data', function (data) {
             data = data.toString();
             assert.equal(true, /^\[\d{2}:\d{2}:\d{2}\.\d{3}\] server : listen http:\/\/\d+\.\d+\.\d+\.\d+:\d+\/\n$/.test(data));
-            index.kill();
+            here.kill();
             done();
         });
     });
     it('`here -v` should output `version`', function (done) {
-        index = spawn('./index.js', ['-v']);
-        index.stdout.on('data', function (data) {
+        here = spawn('node', ['./index.js', '-v']);
+        here.stdout.on('data', function (data) {
             data = data.toString();
             assert.equal(packageJson.version + '\n', data);
             done();
         });
     });
     it('`here --version` should output `version`', function (done) {
-        index = spawn('./index.js', ['--version']);
-        index.stdout.on('data', function (data) {
+        here = spawn('node', ['./index.js', '--version']);
+        here.stdout.on('data', function (data) {
             data = data.toString();
             assert.equal(packageJson.version + '\n', data);
             done();
         });
     });
     it('`here -w` should output `[??:??:??.???] server : listen http://*.*.*.*:*/` and `[??:??:??.???] watcher : listen http://*.*.*.*:*/`', function (done) {
-        index = spawn('./index.js', ['-w']);
+        here = spawn('node', ['./index.js', '-w']);
         var stdoutCount = 0;
-        index.stdout.on('data', function (data) {
+        here.stdout.on('data', function (data) {
             stdoutCount++;
             data = data.toString();
             switch (stdoutCount) {
@@ -49,7 +49,7 @@ describe('test terminal command `here`', function () {
                     break;
                 case 2:
                     assert.equal(true, /^\[\d{2}:\d{2}:\d{2}\.\d{3}\] server : listen http:\/\/\d+\.\d+\.\d+\.\d+:\d+\/\n$/.test(data));
-                    index.kill();
+                    here.kill();
                     done();
                     break;
                 default:
@@ -60,8 +60,8 @@ describe('test terminal command `here`', function () {
         });
     });
     it('`here -h` should output help', function (done) {
-        index = spawn('./index.js', ['-h']);
-        index.stdout.on('data', function (data) {
+        here = spawn('node', ['./index.js', '-h']);
+        here.stdout.on('data', function (data) {
             data = data.toString();
             assert.equal(true, !!~data.indexOf('Usage: index [options]') && !!~data.indexOf('Options:'));
             done();
