d3139e97fb6a007da8764835071aecac8f66981f
vivaxy <xyxuye2007@126.com>
2015-12-21 13:31:21 +0800

    update

--- tool/delete-issue-comment.sh
+++ tool/delete-issue-comment.sh
@@ -4,6 +4,6 @@ while read item
 do
     echo "\n${item}"
     # copy curl in network
-    # replace commentId with ${item}
+    # replace commentId with ${item}, ${item} should be outside of the string
     say ha
 done < list.txt
